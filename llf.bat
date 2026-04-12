40db407d93df40ff06a6356a5ccc4711e64791d6
dev2pew <ermekgd2004@gmail.com>
2026-04-12 15:24:41 +0500

    adjusted reformat script.

--- llf.bat
+++ llf.bat
@@ -5,7 +5,6 @@ ECHO [i] linting and fixing...
 uv.exe run ruff check --fix src/
 
 ECHO [i] formatting...
-uv.exe run ruff format src/
-uv.exe run ruff format tests/
+uv.exe run ruff format src/ tests/
 
 ECHO [+] all done.
